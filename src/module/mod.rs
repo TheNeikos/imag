1fbd624863fff932b7cdb994d74f657c0e97991a
Matthias Beyer <mail@beyermatthias.de>
2016-01-05 22:19:45 +0100

    Fixup 7d2f87f0

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -1,8 +1,4 @@
-use std::collections::HashMap;
-use std::error::Error;
-use std::fmt::{Debug, Display, Formatter};
-use std::fmt::Result as FMTResult;
-use std::result::Result;
+use std::fmt::Debug;
 use std::rc::Rc;
 use std::cell::RefCell;
 
