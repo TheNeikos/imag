adb1bb995ab837e6470dcf1085cb666540c96795
Matthias Beyer <mail@beyermatthias.de>
2016-01-10 15:04:06 +0100

    Add initial implementation

---
+++ libimagrt/src/configuration.rs
@@ -0,0 +1,58 @@
+use std::fmt::{Debug, Formatter, Error};
+use std::path::{Path, PathBuf};
+
+pub use config::types::Config;
+pub use config::reader::from_file;
+
+use log;
+
+pub struct Configuration {
+    pub verbosity: bool,
+    pub editor: Option<String>,
+    pub editor_opts: String,
+}
+
+impl Configuration {
+
+    pub fn new(rtp: &PathBuf) -> Configuration {
+        let cfg = fetch_config(&rtp);
+
+        let verbosity   = cfg.lookup_boolean("verbosity").unwrap_or(false);
+        let editor      = cfg.lookup_str("editor").map(String::from);
+        let editor_opts = String::from(cfg.lookup_str("editor-opts").unwrap_or(""));
+
+        debug!("Building configuration");
+        debug!("  - verbosity  : {:?}", verbosity);
+        debug!("  - editor     : {:?}", editor);
+        debug!("  - editor-opts: {}", editor_opts);
+
+        Configuration {
+            verbosity: verbosity,
+            editor: editor,
+            editor_opts: editor_opts,
+        }
+    }
+
+}
+
+fn fetch_config(rtp: &PathBuf) -> Config {
+    use std::process::exit;
+
+    let mut configpath = rtp.clone();
+    configpath.push("/config");
+    from_file(&configpath).map_err(|e| {
+        println!("Error loading config at '{:?}' -> {:?}", configpath.to_str(), e);
+        println!("Exiting now.");
+        exit(1)
+    }).unwrap()
+}
+
+impl Debug for Configuration {
+
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        try!(write!(f, "Configuration (verbose: {})", self.verbosity));
+        Ok(())
+    }
+
+}
+
--- libimagrt/src/lib.rs
+++ libimagrt/src/lib.rs
@@ -1,3 +1,147 @@
-#[test]
-fn it_works() {
+#[macro_use] extern crate log;
+
+extern crate clap;
+extern crate config;
+
+mod configuration;
+
+use std::path::PathBuf;
+
+pub use clap::App;
+
+use clap::{Arg, ArgMatches};
+use log::{LogLevel, LogLevelFilter, LogRecord, LogMetadata};
+use configuration::Configuration;
+
+struct ImagLogger {
+    lvl: LogLevel,
 }
+
+impl ImagLogger {
+
+    pub fn new(lvl: LogLevel) -> ImagLogger {
+        ImagLogger {
+            lvl: lvl,
+        }
+    }
+
+}
+
+impl log::Log for ImagLogger {
+
+    fn enabled(&self, metadata: &LogMetadata) -> bool {
+        metadata.level() <= self.lvl
+    }
+
+    fn log(&self, record: &LogRecord) {
+        if self.enabled(record.metadata()) {
+            // TODO: This is just simple logging. Maybe we can enhance this lateron
+            println!("[{}]: {}", record.level(), record.args());
+        }
+    }
+}
+
+pub struct Runtime<'a> {
+    rtp: PathBuf,
+    configuration: Configuration,
+    cli_matches: ArgMatches<'a, 'a>,
+}
+
+impl<'a> Runtime<'a> {
+
+    /**
+     * Gets the CLI spec for the program and retreives the config file path (or uses the default on
+     * in $HOME/.imag/config, $XDG_CONFIG_DIR/imag/config or from env("$IMAG_CONFIG")
+     * and builds the Runtime object with it.
+     *
+     * The cli_spec object should be initially build with the ::get_default_cli_builder() function.
+     *
+     */
+    pub fn new(cli_spec: App<'a, 'a, 'a, 'a, 'a, 'a>) -> Runtime<'a> {
+        let matches = cli_spec.get_matches();
+        let rtp : PathBuf = matches.value_of("runtimepath").unwrap_or("~/.imag/").into();
+        Runtime {
+            cli_matches: matches,
+            configuration: Configuration::new(&rtp),
+            rtp: rtp,
+        }
+    }
+
+    /**
+     * appname should be "imag-foo"
+     */
+    pub fn get_default_cli_builder(appname: &'a str,
+                                   version: &'a str,
+                                   about: &'a str)
+        -> App<'a, 'a, 'a, 'a, 'a, 'a>
+    {
+        App::new(appname)
+            .version(version)
+            .author("Matthias Beyer <mail@beyermatthias.de>")
+            .about(about)
+            .arg(Arg::with_name("verbosity")
+                .short("v")
+                .long("verbose")
+                .help("Enables verbosity")
+                .required(false)
+                .takes_value(false))
+
+            .arg(Arg::with_name("debugging")
+                .short("d")
+                .long("debug")
+                .help("Enables debugging output")
+                .required(false)
+                .takes_value(false))
+
+            .arg(Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .help("Path to alternative config file")
+                .required(false)
+                .takes_value(true))
+
+            .arg(Arg::with_name("runtimepath")
+                .short("r")
+                .long("rtp")
+                .help("Alternative runtimepath")
+                .required(false)
+                .takes_value(true))
+    }
+
+    pub fn init_logger(&self) {
+        let lvl = if self.is_debugging() {
+            LogLevelFilter::Debug
+        } else if self.is_verbose() {
+            LogLevelFilter::Info
+        } else {
+            LogLevelFilter::Error
+        };
+
+        log::set_logger(|max_log_lvl| {
+            max_log_lvl.set(lvl);
+            debug!("Init logger with {}", lvl);
+            Box::new(ImagLogger::new(lvl.to_log_level().unwrap()))
+        })
+        .map_err(|e| {
+            panic!("Could not setup logger");
+        });
+    }
+
+    pub fn is_verbose(&self) -> bool {
+        unimplemented!()
+    }
+
+    pub fn is_debugging(&self) -> bool {
+        unimplemented!()
+    }
+
+    pub fn rtp(&self) -> &PathBuf {
+        unimplemented!()
+    }
+
+    pub fn cli(&self) -> &ArgMatches {
+        &self.cli_matches
+    }
+
+}
+
